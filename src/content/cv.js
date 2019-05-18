465d57591c361cc4eee717ebb11bb50e33675c96
Abdul <abdul.rahimi@capgemini.com>
2019-05-18 13:30:13 +0100

    get descripton in homepage

---
+++ src/content/cv.js
@@ -0,0 +1,73 @@
+module.exports = {
+  siteTitle: "Hi!",
+  siteDescription: `Digital curriculum built with Gatsby`,
+  keyWords: ["angular", "react", "curriculum"],
+  authorName: "Abdul Rahimi",
+  twitterUsername: "/",
+  githubUsername: "ab2d2",
+  authorAvatar: "/images/avatar.jpeg",
+  authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
+    In 2012 I started working as a support technician and I approached the developers.
+    <br />
+    I currently work as a frontend developer and mainly work with <strong>Javascript, NodeJS e React.</strong>`,
+  skills: [
+    {
+      name: "HTML",
+      level: 70,
+    },
+    {
+      name: "CSS",
+      level: 60,
+    },
+    {
+      name: "Javascript",
+      level: 50,
+    },
+    {
+      name: "NodeJs",
+      level: 40,
+    },
+    {
+      name: "React",
+      level: 60,
+    },
+    {
+      name: "Git",
+      level: 70,
+    },
+  ],
+  jobs: [
+    {
+      company: "Capgemini",
+      begin: {
+        month: "Sep",
+        year: "2016",
+      },
+      duration: null,
+      occupation: "Frontend developer",
+      description:
+        "I integrate the Frontend team responsible for developing and maintaining the online lending platform.",
+    },
+    /* ... */
+  ],
+  social: {
+    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/in/abdul-rahimi-1678208a",
+    github: "https://github.com/ab2d2",
+    email: "abdulqrahimi@gmail.com",
+  },
+  siteUrl: "https://santosfrancisco.github.io/gatsbystarter-cv",
+  pathPrefix: "/gatsby-starter-cv", // Note: it must *not* have a trailing slash.
+  siteCover: "/images/cover.jpeg",
+  googleAnalyticsId: "UA-131359385-1",
+  background_color: "#ffffff",
+  theme_color: "#25303B",
+  display: "minimal-ui",
+  icon: "src/assets/gatsby-icon.png",
+  headerLinks: [
+    {
+      label: "Francisco Santos",
+      url: "/",
+    },
+  ],
+}
